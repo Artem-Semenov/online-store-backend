f1c7df38e4f75cfd750b79460e1f8eae4825918f
Artem Semenov <artem.semenov.101@gmail.com>
2024-02-05 21:01:54 +0200

    added pagination service

--- src/pagination/pagination.service.ts
+++ src/pagination/pagination.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { PaginationDto } from "src/pagination/dto/pagination.dto";
+import { PrismaService } from "src/prisma.service";
 
 @Injectable()
-export class PaginationService {}
+export class PaginationService {
+  constructor(private prisma: PrismaService) {}
+
+  getPaginations(dto: PaginationDto, defaultPage = 30) {
+    const page = dto.page ? +dto.page : 1;
+    const perPage = dto?.perPage ? +dto.perPage : defaultPage;
+
+    const skip = (page - 1) * perPage;
+
+    return { perPage, skip };
+  }
+}
