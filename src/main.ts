58fbe263aa38e373976648bce67953fb98a87646
Artem Semenov <artem.semenov.101@gmail.com>
2024-02-28 19:16:11 +0200

    upd

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as cookieParser from "cookie-parser";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.setGlobalPrefix("api");
   app.use(cookieParser());
   app.enableCors({
-    origin: ["https://localhost:3000"],
+    origin: ["http://localhost:3000"],
     credentials: true,
     exposedHeaders: "set-cookie",
   });
+  app.disable("x-powered-by");
 
   await app.listen(4200);
 }
